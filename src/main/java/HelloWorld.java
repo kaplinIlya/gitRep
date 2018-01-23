59cee08bb93b7fd13a864b87026eda85c67a0049
Admin <kapli.ilya@bk.ru>
2018-01-23 17:30:21 +0300

    add class

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.app;
+import org.joda.time.LocalTime;
+
+public class HelloWorld {
+    public static void main(String args[]) {
+	LocalTime currentTime = new LocalTime();
+        System.out.println("Hello, new World!!! " + currentTime);
+    }
+}
+
